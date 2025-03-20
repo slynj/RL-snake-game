02792097a3667c5ea51c4305f2e2a0f71c047a05
Lyn <lisa.s.jeong@gmail.com>
2025-03-20 16:45:27 -0400

    created env for abs path

---
+++ env/env.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_PATH = os.path.join(BASE_DIR, "log")
+MODEL_DIR_PATH = os.path.join(BASE_DIR, "best_model")
+MODEL_PATH = os.path.join(MODEL_DIR_PATH, "best_model.zip")
